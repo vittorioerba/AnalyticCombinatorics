776f85695daaafe6fefc08286af7abef6e7e444f
Vittorio Erba <vittorio.erba@posteo.net>
2020-10-22 09:04:06 +0200

    update init

--- Kernel/init.m
+++ Kernel/init.m
@@ -1,1 +1,18 @@
- <<AnalyticCombinatorics`AnalyticCombinatorics`
+(* 
+    Thanks to
+    -   https://github.com/szhorvat/MaTeX/blob/master/MaTeX/Kernel/init.m
+    for the structure of this init file
+*)
+
+
+(* Unprotect package symbols in case package is double-loaded *)
+Unprotect["AnalyticCombinatorics`*"];
+
+(* Load the package *)
+Get["AnalyticCombinatorics`AnalyticCombinatorics`"]
+
+(* Protect all package symbols *)
+SetAttributes[
+  Evaluate@Flatten[Names /@ {"AnalyticCombinatorics`*"}],
+  {Protected, ReadProtected}
+]
