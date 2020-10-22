1c30985c3501d0cc10233df43c22f07a6af8321c
Vittorio Erba <vittorio.erba@posteo.net>
2020-10-22 08:36:11 +0200

    directory structure

---
+++ PacletInfo.m
@@ -0,0 +1,18 @@
+Paclet[
+    Name        -> "AnalyticCombinatorics",
+    Version     -> "0.0.1",
+    Creator     -> "Vittorio Erba <vittorio.erba@posteo.net>",
+    Description -> "A Mathematica paclet to handle asymptotic/singular expansions of generating functions.",
+    Extensions  -> {
+        {"Kernel", 
+            "Root"->".", 
+            "Context"->{"AnalyticCombinatorics`"}
+        },
+        {"Documentation", 
+            "Language" -> "English",
+            "MainPage" -> "Guides/AnalyticCombinatorics",
+            "LinkBase" -> "AnalyticCombinatorics"
+        }
+    }
+]
+
